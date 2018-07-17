259f2db815274dbc6d2cea65ec2075ef99212b22
jamal westfield-kirton <jamalwestfield@gmail.com>
2018-07-17 21:40:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,15 +16,11 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
     if(string === string.toLowerCase()){
-      var lowerCase = string.toLowerCase();
       return ("I can't hear you!");
     }
     else if(string === string.toUpperCase()){
-      var upperCase = string.toUpperCase();
       return("YES INDEED!");
     }
-    else if(string = "I love you, Grandma."){
       var mixedCase = "I love you, too.";
       return(mixedCase);
-  }
 }
